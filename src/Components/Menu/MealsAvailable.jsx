8983fe1e45cd3b5d06b9ac6998e72499ea1c1443
xpknight <xpknight@gmail.com>
2023-07-12 15:25:42 +0530

    Last Patch for Offline Mode

--- src/Components/Menu/MealsAvailable.jsx
+++ src/Components/Menu/MealsAvailable.jsx
@@ -2,39 +2,39 @@ import { useContext } from 'react';
 import Card from '../UI/Card/Card';
 import MealItem from './MealItem/MealItem';
 import classes from './MealsAvailable.module.css';
-const DUMMY_MEALS = [
-    {
-        id: 'm1',
-        name: 'Sushi',
-        description: 'Finest fish and veggies',
-        price: 22.99,
-    },
-    {
-        id: 'm2',
-        name: 'Schnitzel',
-        description: 'A german specialty!',
-        price: 16.5,
-    },
-    {
-        id: 'm3',
-        name: 'Barbecue Burger',
-        description: 'American, raw, meaty',
-        price: 12.99,
-    },
-    {
-        id: 'm4',
-        name: 'Green Bowl',
-        description: 'Healthy...and green...',
-        price: 18.99,
-    },
-    {
-        id: 'm5',
-        name: 'Palak Paneer',
-        description: 'A Healthy Protein filled food',
-        price: 19.99,
-        qty: 0
-    }
-];
+// const DUMMY_MEALS = [
+//     {
+//         id: 'm1',
+//         name: 'Sushi',
+//         description: 'Finest fish and veggies',
+//         price: 22.99,
+//     },
+//     {
+//         id: 'm2',
+//         name: 'Schnitzel',
+//         description: 'A german specialty!',
+//         price: 16.5,
+//     },
+//     {
+//         id: 'm3',
+//         name: 'Barbecue Burger',
+//         description: 'American, raw, meaty',
+//         price: 12.99,
+//     },
+//     {
+//         id: 'm4',
+//         name: 'Green Bowl',
+//         description: 'Healthy...and green...',
+//         price: 18.99,
+//     },
+//     {
+//         id: 'm5',
+//         name: 'Palak Paneer',
+//         description: 'A Healthy Protein filled food',
+//         price: 19.99,
+//         qty: 0
+//     }
+// ];
 
 const MealsAvailable = (props) => {
     // const cartCtx = useContext(CartContext)
